76d26a805c8fa0a16351c01365ff6384e8b05c61
fkrmubaroq <fkrmubaroq@gmail.com>
2025-12-02 16:55:31 +0700

    feat:add layout sidebar

---
+++ frontend/src/app/(auth)/admin/layout.tsx
@@ -0,0 +1,8 @@
+import { AppSidebar } from "@/components/layout/app-sidebar";
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+    return <>
+        <AppSidebar />
+        {children}
+    </>
+}
